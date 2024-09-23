09999700fecf608383c77ab1b274ccfd9d0840b4
Rodrigo Luglio <rodrigo@rlabs.com.br>
2024-09-23 13:46:41 -0300

    change page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,9 @@ function ThemeGenerator() {
   return (
     <section className="p-4">
       <main className="container mx-auto p-4">
-        <h1 className="text-3xl font-bold mb-4">Shadcn/ui Theme Generator</h1>
+        <h1 className="text-3xl font-bold mb-4">
+          RLabs - Shadcn/ui Theme Generator
+        </h1>
 
         <section className="grid grid-cols-1 md:grid-cols-12 w-full gap-4">
           <div className="md:col-span-9">
